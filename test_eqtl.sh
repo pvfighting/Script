c5f785448135b68fc8fa5d162eb7908bd087aa03
Jiajie <jiajiep@gmail.com>
2018-12-19 17:09:56 -0500

    move to O2
    moved to O2eqtl.sh

--- test_eqtl.sh
+++ test_eqtl.sh
@@ -1,11 +1,14 @@
-cd /PHShome/jq34/code
+cd /PHShome/jq34/
 
+ln -s /data/neurogen/MEGA/Plate44-46-May26-2017/MEGAP44-46.GC015.fam /PHShome/jq34/MEGAP44-46.GC015.fam
 ln -s /data/neurogen/MEGA/Plate44-46-May26-2017/MEGAP44-46.GC025.fam /PHShome/jq34/MEGAP44-46.GC025.fam
 cut -d ' ' -f 2 /PHShome/jq34/MEGAP44-46.GC025.fam > /PHShome/jq34/MEGAP44-46_sampleID.GC025.txt
-python sepFirstline.py -i ../genes.fpkm.cufflinks.TCPY.uniq.xls -o ../genes.fpkm.cufflinks.TCPY.uniq.names.txt
+python ./code/sepFirstline.py -i ./genes.fpkm.cufflinks.TCPY.uniq.mergeSameSample.txt -o genes.fpkm.cufflinks.TCPY.uniq.names.txt
 
 cut -d '_' -f 2 /PHShome/jq34/genes.fpkm.cufflinks.TCPY.uniq.names.txt > /PHShome/jq34/temp_sampleID1.txt
 cut -d 'P' -f1 /PHShome/jq34/MEGAP44-46_sampleID.GC025.txt | sed 's/.$//' > /PHShome/jq34/temp_sampleID2.txt
+cut -d 'P' -f2 /PHShome/jq34/MEGAP44-46_sampleID.GC025.txt | cut -d '_' -f1 > /PHShome/jq34/temp_sampleID2_plate.txt
+
 
 sort /PHShome/jq34/temp_sampleID1.txt /PHShome/jq34/temp_sampleID2.txt | uniq -d > /PHShome/jq34/SamplesGenoExpre.txt
 cat /PHShome/jq34/genes.fpkm.cufflinks.TCPY.uniq.names.txt | grep -Ff /PHShome/jq34/SamplesGenoExpre.txt | cut -d '_' --output-delimiter=' '  -f 1,2 > samples.keep.txt
